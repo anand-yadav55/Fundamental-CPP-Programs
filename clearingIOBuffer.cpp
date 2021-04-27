1893bfa1076e50ec6539349f1763b27fa5cc157a
anand-yadav55 <anand55yadav@gmail.com>
2021-04-27 23:01:13 +0530

    clear io Buffer

---
+++ clearingIOBuffer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout << "1st:";
+    cin >> a;
+    cout << "2nd:";
+    cin >> b;
+    c = a + b;
+    cout << "c:" << c;
+    return 0;
+}
